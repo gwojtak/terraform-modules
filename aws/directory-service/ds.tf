95245867c966ececc5c93409a455e57f70dac77c
Greg Wojtak <greg.wojtak@gmail.com>
2024-02-03 12:30:29 -0600

    fix setting edition type in directory services module

--- aws/directory-service/ds.tf
+++ aws/directory-service/ds.tf
@@ -5,17 +5,19 @@ locals {
     ResourceModule = "github.com/gwojtak/terraform-modules//aws/directory-service"
   }
   resolved_tags = merge(local.tags, var.extra_tags)
+  edition       = var.ad_type == "MicrosoftAD" ? var.server_edition : null
 }
 
 resource "aws_directory_service_directory" "this" {
   description = var.description
   name        = var.domain
   password    = var.password
+  edition     = local.edition
   size        = var.directory_size
   type        = var.ad_type
 
   dynamic "vpc_settings" {
-    for_each = var.ad_type != "ADConnector" ? [var.ad_type] : []
+    for_each = var.ad_type != "ADConnector" ? toset([var.ad_type]) : toset([])
 
     content {
       subnet_ids = var.subnet_ids
@@ -24,7 +26,7 @@ resource "aws_directory_service_directory" "this" {
   }
 
   dynamic "connect_settings" {
-    for_each = var.ad_type == "ADConnector" ? [var.ad_type] : []
+    for_each = var.ad_type == "ADConnector" ? toset([var.ad_type]) : toset([])
 
     content {
       customer_dns_ips  = var.customer_dns_ips
@@ -34,5 +36,5 @@ resource "aws_directory_service_directory" "this" {
     }
   }
 
-  tags = local.resolved_tags
+  tags = merge({ Name = var.domain }, local.resolved_tags)
 }
